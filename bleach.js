0f20c43976d96872ff8cc98087cbd0b9983d7404
Daniel W. Robert <danielwrobert@gmail.com>
2015-05-31 23:09:50 -0400

    Began adding scructure for lib.

---
+++ bleach.js
@@ -0,0 +1,11 @@
+var BLEACH = ( function( url, options ) {
+	// Create new XHR instance
+	var xhr = new XMLHttpRequest();
+
+	// Set up optional options data to pass to server
+	options = options || {};
+	options.method = options.method || "GET";
+	options.data = options.data || null;
+
+
+} )( url, options );
